68df1c1abdb1864c7e0e0c9527efc011c764913c
骁勇 <zhouquan.yezq@alibaba-inc.com>
2015-07-19 07:34:25 +0800

    add comment code

---
+++ example/exampleCode.js
@@ -0,0 +1,50 @@
+/* global Prism */
+
+import React from 'react';
+import classNames from 'classnames';
+
+
+let ExampleCode = React.createClass({
+	propTypes: {
+		children: React.PropTypes.string.isRequired
+	},
+	getDefaultProps () {
+		return {
+			language: 'markup'
+		}
+	},
+	componentDidMount () {
+		this.highlight();
+	},
+	componentDidUpdate () {
+		this.highlight();
+	},
+	highlight () {
+		Prism.highlightElement(this.refs.code.getDOMNode(), true);
+	},
+	fixIndentation (children) {
+		if (typeof children !== 'string') return children;
+		var lines = children.split('\n').filter(l => l);
+		if (!lines.length) return children;
+		var indent = /^\t+/.exec(lines[0]);
+		if (indent) {
+			indent = indent[0].length;
+			lines = lines.map(s => s.substr(indent));
+		}
+		return lines.join('\n');
+	},
+	render () {
+		var codeClass = classNames('code-example__code', (
+			'language-' + this.props.language
+		));
+		return (
+			<pre className="code-example__pre">
+				<code ref="code" className={codeClass}>
+					{this.fixIndentation(this.props.children)}
+				</code>
+			</pre>
+		);
+	}
+});
+
+module.exports = ExampleCode;
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: [
         // 'webpack-dev-server/client?http://localhost:9090/assets',
         // 'webpack/hot/only-dev-server',
-        './example/index.jsx'
+        './example/comment.jsx'
     ],
     output: {
         publicPath: 'http://localhost:9090/assets'
